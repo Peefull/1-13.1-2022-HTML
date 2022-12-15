ac97c724b1c7f8132d7f6f4c5931dc42680308f2
Peefull <armand020217@gmail.com>
2022-12-15 11:51:30 +0100

    py

---
+++ ora/ora2.py
@@ -0,0 +1,16 @@
+import math
+
+class Negyzet:
+        def _init_(self, a = 0):
+        self.a = a
+
+        def setA(self, a):
+            self.a = a
+
+        def getKerulet(self):
+            return 4* * self.a 
+
+        def getTerulet(self):
+            return math.pow(self.a, 2)       
+
+
